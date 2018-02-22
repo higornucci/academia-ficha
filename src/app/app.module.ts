75f64033089aeac00de0def4904f850326985df7
Higor Nucci <higornucci@gmail.com>
2018-02-22 19:24:50 -0400

    ionic + materialize + firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseProvider } from './../providers/firebase/firebase';
+
+const config = {
+  apiKey: "AIzaSyDNjh-t_Cofui8milI09FoZ1ku9AbtbNOs",
+  authDomain: "academia-ficha.firebaseapp.com",
+  databaseURL: "https://academia-ficha.firebaseio.com",
+  projectId: "academia-ficha",
+  storageBucket: "academia-ficha.appspot.com",
+  messagingSenderId: "638285993218"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseProvider
+  ]
+})
+export class AppModule {}
